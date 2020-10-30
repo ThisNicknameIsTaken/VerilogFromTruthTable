dc63d06c61cd0352698c6ebf9d0f20d9b88d0316
ThisNicknameIsTaken <32493975+ThisNicknameIsTaken@users.noreply.github.com>
2020-10-31 00:10:39 +0200

    Added new feature
    Now you can create output table for $readmemb with --g argument

--- Testing/TableGenerator.py
+++ Testing/TableGenerator.py
@@ -16,30 +16,29 @@ import random
 
 #---------------------------------------------------------------------------------------INPUT ARGUMENTS
 parser = argparse.ArgumentParser(description="Verilog truth table code generator")
-parser.add_argument("--mode",required=False,default="LittleEndian", help = "(Optional, LittleEndian by default) Output mode.")
+
+parser.add_argument("--mode",required=False, default=False,help = "(Optional, LittleEndian by default) Output mode. BigEndian or LittleEndian.", type=bool)
 parser.add_argument("--po",required=True,help="Path to generated table")
 parser.add_argument("--i",required=True,help="Amount of input signals",type=int)
 parser.add_argument("--o",required=True,help="Amount of output signals",type=int)
+parser.add_argument("--g",required=False,default=1,help="(Optional, by default is generating) Generate output table for $readmemb. input 1 to generate this table, 0 - stop generation",type=int)
 
 console_arguments = parser.parse_args()
 
-BigEndian = False
-mode = console_arguments.mode
-if(mode == "BigEndian"):
-    BigEndian = True
-if(mode != "LittleEndian" and mode != "BigEndian"):
-    raise Exception("Wrong input of --mode argument")
-    
+BigEndian = console_arguments.mode
 full_output_path = console_arguments.po
 
 input_amount = console_arguments.i
 if(input_amount <= 0):
-    raise Exception("Your input should be greater than 0!\n")
+    raise Exception("--i; Your input should be greater than 0!\n")
 
 output_amount = console_arguments.o
 if(output_amount <= 0):
-    raise Exception("Your input should be greater than 0!\n")
+    raise Exception("-- o; Your input should be greater than 0!\n")
 
+allow_generation = console_arguments.g
+if(allow_generation != 1 and allow_generation != 0):
+    raise Exception("--g; Wrong argument input! Only 1 and 0 are allowed")
 
 #Checking output file parameters
 splited_out_path = full_output_path.split("\\")
@@ -76,6 +75,8 @@ random.seed()
 
 full_combination_amount = math.pow(2,input_amount)                         #Amount of lines in file to get all combinations
 
+
+testbench_readmemb = ""
 
 with open(full_output_path,'w') as file:
     first_line_counter = 1
@@ -115,12 +116,25 @@ with open(full_output_path,'w') as file:
 
         out_counter = 0
         while out_counter < output_amount:
-            file.write(str(random.randint(0,1)) + "\t")
+            num = random.randint(0,1)
+            testbench_readmemb += str(num)
+            file.write(str(num) + "\t")
             out_counter += 1
         
         line_counter += 1
+        testbench_readmemb += "\n"
         file.write("\n")
 
                 
+ #Generate comfotable table for $readmemb               
+if(allow_generation):
 
+    #Create new file with name of original table + _testbench_table
+    #Result of this operation is such: usersTableName_testbencj_table.txt
+    splited_out_path = full_output_path.split(".")
+    splited_out_path[0] += "_testbench_table"
+    new_path = splited_out_path[0] + "." + splited_out_path[1]
+
+    with open(new_path,'w') as file:
+        file.write(testbench_readmemb)
     
